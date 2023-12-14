e0a77595c553ead72fb5a2ee45f02f552abe9f71
Axel Demborg <axel.demborg@volumental.com>
2023-12-14 10:49:11 +0100

    use dimitris to actually compute the correct tone

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,13 @@ import { describe, it, expect } from 'vitest';
 import { frequencyToNoteName } from '$lib/toneTools';
 
 describe('Frequency to tone test', () => {
-	it('tests C#4', () => {
-		expect(frequencyToNoteName(440)).toBe('C#4');
-	});
+	it('tests a/4', () => {
+		expect(frequencyToNoteName(440)).toBe('a/4');
+	})
+	it('tests c/5', () => {
+		expect(frequencyToNoteName(523.25)).toBe('c/5');
+	})
+	it('tests e/5', () => {
+		expect(frequencyToNoteName(659.25)).toBe('e/5');
+	})
 });
--- src/lib/toneTools.ts
+++ src/lib/toneTools.ts
@@ -1,16 +1,16 @@
-export function frequencyToNoteName(frequency: number): string {
-	const A4 = 440;
-	const A4_INDEX = 49; // A4 is the 49th key on the piano
-	const SEMITONES_IN_OCTAVE = 12;
+const A4 = 440;
+const A4_INDEX = 49; // A4 is the 49th key on the piano
+const SEMITONES_IN_OCTAVE = 12;
+const NOTE_NAMES = [ 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#'];
 
-	let n = SEMITONES_IN_OCTAVE * Math.log2(frequency / A4);
+export function frequencyToNoteName(frequency: number): string {
+	let n = SEMITONES_IN_OCTAVE * Math.log2(frequency / A4) + A4_INDEX;
 	n = Math.round(n); // Round to nearest semitone
 
-	const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
 	// Calculate the note index and octave
-	const noteIndex = (A4_INDEX + n) % SEMITONES_IN_OCTAVE;
-	const octave = Math.floor((A4_INDEX + n) / SEMITONES_IN_OCTAVE);
+	const noteIndex = (n - 1) % SEMITONES_IN_OCTAVE;
+	const octave = Math.floor((n + 8) / SEMITONES_IN_OCTAVE);
 
-	return noteNames[noteIndex] + octave;
+	return NOTE_NAMES[noteIndex] + "/" + octave;
 }
