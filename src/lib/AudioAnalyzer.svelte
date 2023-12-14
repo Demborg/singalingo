cf83cf3120c13b2b0208e14bb48c7de5b3145b7e
Axel Demborg <axel.demborg@volumental.com>
2023-12-14 17:19:47 +0100

    Add more lessons

--- src/lib/AudioAnalyzer.svelte
+++ src/lib/AudioAnalyzer.svelte
@@ -9,8 +9,12 @@
 	let dataArray: Uint8Array;
 	let dominantFrequency: number = 0;
 	const levels = [
-		['c/4', 'c/5', 'a/4', 'g/4'],
-		['d/4', 'c#/5', 'a#/4', 'a#/4']
+		['c/4', 'd/4', 'e/4', 'f/4'],
+		['g/4', 'a/4', 'b/4', 'c/5'],
+		['c/5', 'b/4', 'a/4', 'g/4'],
+		['f/4', 'e/4', 'd/4', 'c/4'],
+		['c/4', 'e/4', 'g/4', 'c/5'],
+		['c/5', 'g/4', 'e/4', 'c/4'],
 	];
 	let level = 0;
 	let current_note_index = 0;
